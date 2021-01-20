37226f1d5a15a63ad3597c46b460226291cfd4ec
BuddhiWathsala <b.wathsala.bw@gmail.com>
2021-01-20 20:25:31 +0530

    Rename ballerina->ballerinax and jarrays->java.arrays

--- java.arrays-test-utils/src/main/java/module-info.java
+++ java.arrays-test-utils/src/main/java/module-info.java
@@ -16,7 +16,7 @@
  * under the License.
  */
 
-module io.ballerina.stdlib.jarrays.testutils {
+module io.ballerina.stdlib.java.arrays.testutils {
     requires io.ballerina.runtime;
-    exports org.ballerinalang.stdlib.jarrays.testutils;
+    exports org.ballerinalang.stdlib.java.arrays.testutils;
 }
